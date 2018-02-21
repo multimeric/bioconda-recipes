8e59c22b8a3b894792b050437b884138d7f8ccc6
Michael Milton <ttmigueltt@gmail.com>
2018-02-21 18:32:14 +1100

    Build fixes

--- recipes/penncnv/build.sh
+++ recipes/penncnv/build.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-sed -i -e '/#undef bool/d' khmm_wrap.c
 pushd kext
+    sed -i -e '/#undef bool/d' khmm_wrap.c # Fix syntax
     rm -rf *.*.* # cleanup the precompiled Windows libraries
-    make
+    CFLAGS=-fPIC make
     cp *.*.*/* $PREFIX/lib # Move kext into lib
 popd
 cp *.pl $PREFIX/bin
